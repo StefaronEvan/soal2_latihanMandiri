a3292505ebcacd3817d85177cf8cfc3d1733fd8e
StefaronEvan <103369000+StefaronEvan@users.noreply.github.com>
2022-04-21 12:48:32 +0700

    Add files via upload

---
+++ soal2.py
@@ -0,0 +1,43 @@
+#membuka file
+file = open('data_soal2.txt')
+
+#inisiasi variable
+hasil = ''
+soal = ''
+kunci = ''
+
+#untuk mengcek setiap baris dalam file
+for line in file:
+
+    #memisahkan dengan karakter '||'
+    hasil = line.split('||')
+
+    #memasukkan soal
+    soal = hasil[0]
+
+    #memasukkan kunci
+    kunci = hasil[1]
+    kunci = kunci.rstrip()
+    kunci = kunci.lstrip()
+
+    #untuk mengatasi sensitive case
+    kunci = kunci.lower()
+
+    #cetak soal
+    print(soal, end='\n')
+
+    #user menginputkan jawaban
+    jawab = input('Jawab :')
+
+    #supaya sama dengan kunci (mengastasi sensitive case)
+    jawab = jawab.lower()
+
+    #mengcek jawaban apakah benar
+    if jawab == kunci:
+        print('Jawaban Benar !')
+    else :
+        print('Jawaban salah !')
+
+
+'''SEKIAN DAN TERIMAKASIH DARI SAYA, KALAU MASIH EROR BERARTI DEVICE ANDA KUNO:)'''
+
